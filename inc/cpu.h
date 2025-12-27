91200b68f5519cd8869eca5526253d29ce7eb947
Ray <reinamlaura@gmail.com>
2025-12-27 18:04:19 -0400

    feat: add CPU tests and definitions

--- inc/cpu.h
+++ inc/cpu.h
@@ -1,12 +1,47 @@
-#ifndef MOCK_CPU_H
-#define MOCK_CPU_H
+#ifndef CPU_H
+#define CPU_H
 
 #include "../inc/definitions.h"
 
+/**
+ * @brief CPU Status Codes.
+ * Used to indicate the result of CPU operations.
+ */
+typedef enum {
+	CPU_OK = 0,          /**< CPU operation successful */
+    CPU_HALT = 1,        /**< CPU has reached a halt instruction */
+} CPUStatus_t;
+
+/**
+ * @brief Fetches the next instruction from memory into the IR.
+ */
+CPUStatus_t fetch(void);
+
+/**
+ * @brief Decodes the instruction in the IR into its components.
+ */
+Instruction_t decode(void);
+
+/**
+ * @brief Executes the given instruction.
+ *
+ * @param instruction The instruction to execute
+ */
+CPUStatus_t execute(Instruction_t instruction);
+
+/**
+ * @brief Runs the CPU until a halt condition is met.
+ */
 int cpuRun(void);
 
+/**
+ * @brief Executes a single instruction.
+ */
 bool cpuStep(void);
 
+/**
+ * @brief Resets the CPU to its initial state.
+ */
 void cpuReset(void);
 
-#endif // MOCK_CPU_H
+#endif // CPU_H
---
+++ test/test_cpu.c
@@ -0,0 +1,38 @@
+#include "../lib/utest.h"
+#include "../inc/cpu.h"
+#include "../inc/memory.h"
+
+CPU_t CPU;
+word RAM[RAM_SIZE];
+
+//Mock function for writing in memory
+MemoryStatus_t writeMemory(address addr, word value) {
+	if (addr >= OS_RESERVED_SIZE && addr < RAM_SIZE) {
+		RAM[addr] = value;
+		return 0;
+	}
+	return 1;
+}
+
+UTEST_MAIN();
+
+// Verify that the fetch stage correctly loads instruction into IR
+UTEST(CPU, FetchStage) {
+    writeMemory(300, 04100005);
+    CPU.PSW.pc = 300;
+    fetch();
+    ASSERT_EQ(CPU.MAR, 300);
+    ASSERT_EQ(CPU.MDR, 04100005);
+    ASSERT_EQ(CPU.IR, 04100005);
+    ASSERT_EQ(CPU.PSW.pc, 301);
+}
+
+// Verify that the decode stage correctly interprets the instruction in IR
+UTEST(CPU, DecodeStage) {
+    CPU.IR = 04100005;
+    Instruction_t inst;
+    inst = decode();
+    ASSERT_EQ(inst.opCode, OP_LOAD);
+    ASSERT_EQ(inst.direction, DIR_DIRECT);
+    ASSERT_EQ(inst.value, 5);
+}
