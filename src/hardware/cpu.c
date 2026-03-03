72db24c5bbebf6dd2052ae38eaeaced04be96951
Druxorey <druxorey@gmail.com>
2026-03-02 23:26:25 -0400

    feat(scheduler): implement round-robin scheduler:
    - Added context switching logic between processes
    - Integrated `osYield` flag for timer-based preemption
    - Updated CPU loop to handle process termination and yields
    - Implemented sleep/wake mechanism in `schedulerTick`

--- src/hardware/cpu.c
+++ src/hardware/cpu.c
@@ -5,9 +5,10 @@
 #include <stdbool.h>
 
 #include "../../inc/logger.h"
+#include "../../inc/console.h"
 #include "../../inc/hardware/cpu.h"
 #include "../../inc/hardware/memory.h"
-#include "../../inc/kernel/scheduler.h"
+#include "../../inc/kernel/core.h"
 
 static uint16_t interruptBitmap = 0;
 static int64_t interruptValue = 0;
@@ -137,6 +138,7 @@ bool checkInterrupts(void) {
 
 bool handleInterrupt(InterruptCode_t code) {
 	int syscallCode;
+	char messageBuffer[LOG_BUFFER_SIZE];
 	switch (code) {
 		case IC_INVALID_INSTR:
 			loggerLogHardware(LOG_ERROR, "Invalid Instruction: CPU Halt triggered");
@@ -153,9 +155,9 @@ bool handleInterrupt(InterruptCode_t code) {
 			CPU.AC = intToWord(0, &CPU.PSW);
 			loggerLogHardware(LOG_INFO, "Arithmetic Underflow: Value clamped to 0");
 			return true;
-		case IC_TIMER: // Simulate program pause
-			schedulerTick();
+		case IC_TIMER:
 			loggerLogHardware(LOG_INFO, "Timer Interrupt: External clock tick received");
+			osYield = true;
 			return true;
 		case IC_IO_DONE: // Simulate program resume after I/O completion
 			loggerLogHardware(LOG_INFO, "I/O Interrupt: Peripheral operation completed");
@@ -164,13 +166,15 @@ bool handleInterrupt(InterruptCode_t code) {
 			syscallCode = wordToInt(CPU.AC);
 			if (syscallCode == 0) {
 				loggerLogHardware(LOG_INFO, "SYSCALL [0]: Program requested EXIT");
-				printf("SYSTEM CALL [0]: Program requested termination (EXIT).\n");
+				monitorPrint("SYSTEM CALL [0]: Program requested termination (EXIT)");
 				return false;
 			} else {
 				// Any other code is considered a service request that does NOT stop the CPU
 				snprintf(logBuffer, LOG_BUFFER_SIZE, "SYSCALL [%d]: Service requested by process", syscallCode);
 				loggerLogHardware(LOG_INFO, logBuffer);
-				printf("SYSTEM CALL [%d]: Service handled (Simulation).\n", syscallCode);
+
+				snprintf(messageBuffer, LOG_BUFFER_SIZE, "SYSTEM CALL [%d]: Service handled (Simulation)", syscallCode);
+				monitorPrint(messageBuffer);
 				// In the future an interrupt will be generated here, but the CPU does NOT stop,
 				// it simply continues to the next instruction after being serviced.
 				return true;
